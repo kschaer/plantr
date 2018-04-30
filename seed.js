62a9db318897168885cbb266f839405b463d0bdb
Ganchimeg Ganzorig <ganchimeg10@gmail.com>
2018-04-30 18:04:43 -0400

    done

--- seed.js
+++ seed.js
@@ -13,15 +13,28 @@ const {db, Gardener, Vegetable, Plot} = require('./models');
 
   db.sync({ force: true })
   .then(() => {
-    return Vegetable.create({
+    const vegetable1 = Vegetable.create({
       name: 'Bob',
       color: 'Orange',
       planted_on: new Date(Date.now())
     });
+    return vegetable1;
+  })
+  .then((vegetable1) => {
+    const newPlot = Plot.create({
+      size: 10,
+      shaded: true
+    });
+    //in this scope there are two promises
+    return Promise.all([vegetable1, newPlot]);
+  })
+  .then(([vegetable1, newPlot]) => {
+    return vegetable1.setPlots([newPlot]);
   })
   .then(() => {
     db.close();
   })
   .catch(err => {
+    db.close();
     console.log(err);
   });
